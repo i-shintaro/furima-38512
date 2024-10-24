745c77aa692d8efe4f4fd603084c17ba1758da81
i-shintaro <shintaroiwashita7857@gmail.com>
2024-10-24 15:34:51 +0900

    active_hash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :item_name, numericality: { other_than: 1, message: "can't be blank" }
+    validates :description, numericality: { other_than: 1, message: "can't be blank" }
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
---
+++ app/models/shipping_day.rb
@@ -0,0 +1,10 @@
+class Shippingday < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '1~2日で発送' },
+    { id: 2, name: '2~3日で発送' },
+    { id: 2, name: '3~4日で発送' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
