e846ff7c36a5ab94480048de98198eccf31c240b
i-shintaro <shintaroiwashita7857@gmail.com>
2024-11-27 11:19:02 +0900

    アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
               format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must be a mixture of letters and numbers.' }
   end
   has_many :items
+  has_many :orders
 end
