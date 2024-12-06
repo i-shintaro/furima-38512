d287474bdf4686d11a33b3efc4dba13db770ba1d
i-shintaro <shintaroiwashita7857@gmail.com>
2024-12-06 15:35:55 +0900

    item_idバリデーション追加

--- app/models/shippings_order.rb
+++ app/models/shippings_order.rb
@@ -4,6 +4,7 @@ class ShippingsOrder
 
   with_options presence: true do
     validates :user_id
+    validates :item_id
     validates :token, presence: true
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
